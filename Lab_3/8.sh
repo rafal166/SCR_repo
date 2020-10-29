66112bf3b01197c295ed8d9e0fb5bb678dae5196
Rafal <rafal.rzewucki@gmail.com>
2020-10-29 08:41:20 -0700

    Update ex8 in lab_3

--- Lab_3/8.sh
+++ Lab_3/8.sh
@@ -1,9 +1,16 @@
 #!/bin/bash
+#sprzatac chcialem komenda:
+# kill -9 $(pgrep -u user -n "8\.*")
+#
+# jednak dostawalem taki komunikat gdy chcialem posprzatac
+#bash: fork: retry: Resource temporarily unavailable
+# wiec nie udalo sie posprzatac
 
 echo $$;
 echo
 echo "Program zbyt niebezpieczny. Nie uruchamiac dalej jesli nie wiesz co robisz";
 while [ true ]; do
 	./8.sh
+	echo hej;
 	sleep 3;
 done
