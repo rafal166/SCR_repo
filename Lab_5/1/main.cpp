fe57e46dbe5635a3dff9e77621bc0bdad26e831f
Rafal <rafal.rzewucki@gmail.com>
2020-12-07 01:45:41 -0800

    Lab_6

--- Lab_5/1/main.cpp
+++ Lab_5/1/main.cpp
@@ -18,14 +18,17 @@ int main(int argc, char *argv[])
 		cout << "Nie udalo sie otworzyc potoku" << endl;
 		return 1;
 	}
-
-	switch (fork())
+	int i = 0;
+	int jakis_fork = fork();
+	
+	switch (jakis_fork)
 	{
 	case -1:
 		cout << "Error" << endl; // nie udalo sie utworzyc potomka
 		exit(1);
 	case 0:
 		// potomek
+		cout << "potomek";
 		close(pipe_fds[1]); // zamykanie potoku do zapisu
 
 		while ((size = read(pipe_fds[0], inbuf,
@@ -38,12 +41,13 @@ int main(int argc, char *argv[])
 		break;
 	default:
 		//rodzic
+		cout << "rodzic";
 		close(pipe_fds[0]); // zamykanie potoku do odczytu
 		char tmp;
 
 		// otwieram plik do odczytu
 		fstream file(argv[1], fstream::in); // otwarcie pliku do odczytu
-		while (file >> noskipws >> tmp) // czytanie z pliku znak po znaku
+		while (file >> noskipws >> tmp)		// czytanie z pliku znak po znaku
 		{
 			write(pipe_fds[1], &tmp, sizeof tmp);
 		}
--- Lab_6/main.cpp
+++ Lab_6/main.cpp
@@ -1,30 +1,50 @@
-#include <stdlib.h>
-#include <stdio.h>
-#include <string.h>
+#include <iostream>
+#include <fstream>
 #include <unistd.h>
 #include <fcntl.h>
-#include <errno.h>
-#include <sys/types.h>
+#include <sys/stat.h>
 #include <sys/mman.h>
-#define SHM_SEGMENT "/shm_segment"
+#define PLIK_WSPOLDZIELONY "pamiec"
 #define MODES 0666
-typedef struct
-{
-    int client_wrote;
-    char text[BUFSIZ];
-} shared_struct;
-int main()
-{
-    int shmd;
-    // shared_struct *segment;
 
-    shmd = shm_open(SHM_SEGMENT, O_RDWR, MODES);
-    ftruncate(shmd, 1000);
-    void * segment =
-        mmap(NULL, 1000, PROT_WRITE,
-             MAP_SHARED, shmd, 0);
-    sprintf(segment, "Hello, world.");
+using namespace std;
 
-    munmap((char *)segment, 1000);
+int main(int argc, char *argv[])
+{
+    int fd, i = 0;
+    char nazwa_pliku[100], tmp;
+    struct stat FileStat;
+    char *pamiec;
+
+    fd = open(PLIK_WSPOLDZIELONY,
+              O_RDWR | O_CREAT, // odczyt/zapis, jesli nei ma to uworz
+              S_IRWXU | S_IROTH | S_IWOTH | S_IXOTH); // utworzy z takimi prawami
+
+    while (1)
+    {
+        cout << "Nazwa pliku: ";
+        cin >> nazwa_pliku;
+        if (nazwa_pliku[0] == 'q')
+            break;
+
+        stat(nazwa_pliku, &FileStat); // pobieranie informacji o pliku
+        pamiec = (char *)mmap(NULL,
+                              FileStat.st_size,
+                              PROT_READ | PROT_WRITE,
+                              MAP_SHARED,
+                              fd, 0);    // mapowanie pamieci w programie
+        ftruncate(fd, FileStat.st_size); // zmiana rozmaru pliku na dysku
+
+        fstream file(nazwa_pliku, fstream::in); // otwarcie pliku do odczytu
+        i = 0;
+        while (file >> noskipws >> tmp) // czytanie z pliku znak po znaku
+            pamiec[i++] = tmp;          // zapisywanie znaku do pliku i zwiekszanie wskaznika pamieci
+
+        cout << "Pomyslnie zapisano do pliku!" << endl;
+    }
+
+    ftruncate(fd, 0);                         // czyszczenie pliku na dysku
+    munmap((void *)pamiec, FileStat.st_size); // zwalnianie pamieci w programie
+    close(fd);                                // zamykanie pliku pamieci wspoldzielonej
     return 0;
 }
